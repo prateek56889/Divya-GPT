ec812324747bfa33610a71ea84d2f1dbc882d2c4
prateek56889 <165522973+prateek56889@users.noreply.github.com>
2024-04-14 19:36:57 +0530

    Update divyagpt.py

--- divyagpt.py
+++ divyagpt.py
@@ -1,6 +1,8 @@
 import streamlit as st
 import google.generativeai as model
+from io import BytesIO
 model.configure(api_key=st.secrets["GEN_API"])
+
 def prompts():
      return """
      Hi buddy i am giving you some names you have to filter them out and generate accordingly
@@ -11,6 +13,16 @@ def prompts():
      5. glossary or hard words 
      6. questions and answers upto 5 
      """
+def flow_char(title,over):
+    graph = Digraph()
+    # Nodes ko add karein
+    graph.node(name=title,style="filled",color="cyan")
+    for j in over.split(","):
+        graph.node(name=j[:80],style="filled",color="green")
+    for j in over.split(","):
+        graph.edge(title,j[:80])
+    # Graph ko visualize karein aur save karein
+    return graph
 def gemini_res(input,prompt):
     """
     this has to be get in return through gemini
@@ -20,7 +32,7 @@ def gemini_res(input,prompt):
     return res.text
 st.set_page_config(page_title="Divya GPT",page_icon="div.png")
 st.image("divya.png")
-inp=st.text_area(label="Enter the topic to generate")
+inp=st.text_area(label="Enter the Prompt")
 response=gemini_res(input=prompts(),prompt=inp)
 gen_per=st.button(label="Generate")
 if gen_per==True:
@@ -28,16 +40,17 @@ if gen_per==True:
      st.sidebar.image("favicon.svg")
      st.sidebar.title("Wanna download your important Topics ?")
      st.sidebar.subheader("DivyaGPT offers Download the chat option")
-     with open("summary.txt","w") as data:
-               data.write(response)
-               st.sidebar.download_button("Download Summary",response,file_name="summarise.txt")
+     st.sidebar.download_button("Download Chats",response,file_name="summarise.txt")
      st.sidebar.title("Get's hard to understand Text?")
      st.sidebar.subheader("Dont'worry Divya gpt too provides Ascii based flowcharts , Wanna check?")
-     flow=gemini_res(input="""can you create an ascii based flowchart for the following data make
-                     sure that flowchart is comprehensive and attractive""",prompt=inp)
-     with open("flow.txt","w") as flo:
-           flo.write(flow)
+     flow=gemini_res(input="""According to the input make sure to mention some key points around 5 key points,
+                     each point should be seperated by comma ',' dont mention numbers only a paragraph seperated by comma""",prompt=inp)
+     with open("flow.jpg","wb") as flo:
            st.write(flow)
-           st.sidebar.download_button("Download Flowcharts",flow,file_name="Flowchat.txt")
+           flowchar=flow_char(title=inp,over=flow)
+           flowchart_bytes = BytesIO()
+           flowchart_bytes.write(flowchar.pipe(format='png'))
+           flowchart_bytes.seek(0)
+           st.sidebar.download_button("Download Flowcharts",flowchart_bytes.getvalue(),file_name="Flowchat.jpg",mime="image/png")
      
-st.write("Note:-Divya GPT sometimes create Biased data if any thing persists make sure to regenerate the data")
+st.write("NOte:-Divya Gpt can make mistakes, retry the prompt is issue engagges")
